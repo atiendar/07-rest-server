352f81b3a4d63c83c3e8062e132a74bbad2b11c8
Marco Tienda <marco.tiendar@gmail.com>
2021-12-29 01:29:23 -0600

    Obtener datos: Se obtienen datos sin problema

--- models/server.js
+++ models/server.js
@@ -18,6 +18,9 @@ class Server {
         // * CORS
         this.app.use(cors())
 
+        // * Lectura y parseo del middleware
+        this.app.use(express.json())
+
         this.app.use(express.static('public')) // ! <- Denegando el acceso a la carpeta pública
     }
 
